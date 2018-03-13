3aa4e83b28e46b940c8c2700b46442fbd26fb04e
Chloe Lemaire <clemaire@csumb.edu>
2018-03-13 15:45:08 +0000

    add

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
        <a href= "/Homework/Homework1"> Homework1 </a>
        <a href= "/Lab2/777"> Lab2 </a>
      <a href= "/Homework/Homework2"> Homework 2 </a>
+     <a href= "/Lab3"> Lab3 </a>
         </main>
         
         <footer>
